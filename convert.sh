25cb9fba0ee7820a79a77f4815d21316214db13a
Vladimir-pa <vov4ikpa@gmail.com>
2019-06-13 16:15:49 +0300

    Lecture 11

--- convert.sh
+++ convert.sh
@@ -4,11 +4,29 @@ path="./Lectures-lyx"
 dest="./Lectures-pdf"
 format="pdf4"
 ext="pdf"
-files=`ls $path | grep -v \~ | grep -v ^[1-9] | cut -d"." -f1 | uniq`
-for f in $files
-do
-    if [ -f  $f ]; then
+
+lecfiles=`ls $path | grep -v \~ | grep -v ^[1-9] | grep algo | cut -d"." -f1 | uniq | sort -V`
+
+make_lectures() {
+    for f in $lecfiles
+    do
         lyx -e $format $path/$f.lyx
         mv -f $path/$f.$ext $dest/
-    fi
-done
+    done
+    lyx -e pdf2 $path/algo10.lyx
+    mv -f $path/algo10.$ext $dest/
+    lyx -e pdf2 $path/algo11.lyx
+    mv -f $path/algo11.$ext $dest/
+}
+
+make_merged_lectures() {
+    sources=`ls $dest | grep algo | sort -V`
+    ls $dest
+    args=`echo $sources Lectures.pdf`
+    cd $dest
+    pdfunite $args
+    cd ..
+}
+
+make_lectures
+make_merged_lectures
